67d7dea0ad82ee1c20d58286b872228e46d8c6ee
yaoxinc <yaoxinc@yonyou.com>
2017-01-17 13:59:22 +0800

    具体代码

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/**
+ * Module : severEvent webpack entry index
+ * Author : yaoxinc
+ * Date	  : 2017-01-17 11:25
+ */
+
+import {
+    ServerEvent
+} from './indexServerEvent';
+window.ServerEvent = ServerEvent;
